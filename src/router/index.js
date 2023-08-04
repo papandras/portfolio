f305d2be27f0e9479df4b14257a5c2558efc790d
papandras <papandras01@gmail.com>
2023-08-04 21:30:18 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useStore } from '../stores/lang.js'
 import HomeView from '../views/HomeView.vue'
 import AdminView from '../views/AdminView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
