96a7a4b089c751371849071ffa13dfad0916ff58
papandras <papandras01@gmail.com>
2023-08-04 21:07:49 +0200

    Update index.js
    remove env from history creator

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import AdminView from '../views/AdminView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
